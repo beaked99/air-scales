fff81c602da38d7568103d7fdc2f564b86f36463
beaker <kwiebe0@gmail.com>
2025-07-03 19:21:52 -0700

    pwa time

---
+++ public/app/sw.js
@@ -0,0 +1,47 @@
+// public/app/sw.js
+
+const CACHE_NAME = 'air-scales-cache-v1';
+const FILES_TO_CACHE = [
+  '/app/',
+  '/app/index.html',
+  '/app/sw.js',
+  '/app/manifest.webmanifest',
+  '/pwa/icon-192.png',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[Service Worker] Pre-caching offline resources');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[Service Worker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
