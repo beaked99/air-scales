4120e7b2fabfdc131fe651a3225fd2a23fe47906
beaker <kwiebe0@gmail.com>
2025-07-07 23:49:02 -0700

    adding shit to make js load???

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -30,4 +30,23 @@ class AppController extends AbstractController
             'build_date' => $versionData['build_date'],
         ]);
     }
+    #[Route('/app/version.json', name: 'app_version_json', methods: ['GET'])]
+    public function versionJson(): Response
+    {
+        $versionFile = __DIR__ . '/../../public/app/version.json';
+        $versionData = [
+            'version' => 'dev',
+            'build_date' => (new \DateTime())->format('Y-m-d'),
+        ];
+
+        if (file_exists($versionFile)) {
+            $json = file_get_contents($versionFile);
+            $decoded = json_decode($json, true);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $versionData = array_merge($versionData, $decoded);
+            }
+        }
+
+        return $this->json($versionData);
+    }
 }
